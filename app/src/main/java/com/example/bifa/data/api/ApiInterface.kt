b86f869765b0e92af541b1471a171e261673f51d
Jose Arenas <arenas782@gmail.com>
2020-12-12 11:05:31 -0400

    initial commit

---
+++ app/src/main/java/com/example/bifa/data/api/ApiInterface.kt
@@ -0,0 +1,7 @@
+package com.example.bifa.data.api
+
+/*
+ Created by arenas on 12/12/20.
+*/
+class ApiInterface {
+}
---
+++ app/src/main/java/com/example/bifa/data/model/category/Category.kt
@@ -0,0 +1,10 @@
+package com.example.bifa.data.model.category
+
+/*
+ Created by arenas on 12/12/20.
+*/
+
+class Category {
+
+    
+}
